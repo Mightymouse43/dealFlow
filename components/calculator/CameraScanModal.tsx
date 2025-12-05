1fe16aa6b5f8085b8aeaf379e336d1118a053971
Mightymouse43 <89362913+Mightymouse43@users.noreply.github.com>
2025-12-05 13:45:17 -0800

    Updated CameraScanModal.tsx

--- components/calculator/CameraScanModal.tsx
+++ components/calculator/CameraScanModal.tsx
@@ -255,7 +255,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.3)',
   },
   header: {
-    paddingTop: 50,
+    paddingTop: 70,
     paddingHorizontal: 20,
     paddingBottom: 10,
     flexDirection: 'row',
